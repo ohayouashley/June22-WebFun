c886b43c6142f6e851a1b440a32f72be78658e89
Ashley Chase <chaseashley91@gmail.com>
2022-06-21 17:17:06 -0700

    examprep practice

---
+++ assignments/week3/workspace3/functions/functions1.js
@@ -0,0 +1,12 @@
+/* 
+
+Functions. 
+
+** No two functions can be named the same thing. However they can be used more than once.
+Should in some way describe the idea of the function. myFunction is not a good name. 
+
+
+
+
+
+*/
